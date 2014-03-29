c7629b631b27bb91a2de70dd8f6c29dce5018af4
marciocamello <marciocamello@outlook.com>
2014-03-29 07:50:42 -0300

    Action

---
+++ XEditableAction.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace mcms\xeditable;
+
+use yii\base\Action;
+use yii\helpers\VarDumper;
+use yii\web\NotFoundHttpException;
+
+class XEditableAction extends Action
+{
+	public $modelclass;
+	public $scenario='';
+
+	public function run()
+	{
+		if(\Yii::$app->request->getIsPost()){
+			$pk=$_POST['pk'];
+			$name=$_POST['name'];
+			$value=trim($_POST['value']);
+			$modelclass=$this->modelclass;
+			$model= $modelclass::find($pk);
+			if($this->scenario){
+				$model->setScenario($this->scenario);
+			}
+			if($model===null)
+				throw new NotFoundHttpException();
+			$model->$name = $value;
+			if ($model->validate()){
+				$model->update();
+			}else{
+				VarDumper::dump($model->getErrors(),10);
+			}
+		}
+	}
+}
